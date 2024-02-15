376f1fb2c5b074ba12a0ec8ae03506ef7c852414
Herlandro Tribiakto <herlandrotri@gmail.com>
2024-02-15 15:18:36 +0900

    add: react query provider and provider component on layout

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,5 +1,21 @@
 'use client'
 
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import React, { ReactNode } from 'react'
+
+interface Props {
+    children?: ReactNode
+}
+
+export default function Providers({ children }: Props) {
+    const [queryClient] = React.useState(() => new QueryClient())
+
+    return (
+        <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    )
+}
+
 // import { SessionProvider } from "next-auth/react"
 // import React, { ReactNode} from "react"
 
